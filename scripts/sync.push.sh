98b40be19544e154639c94232509e52de05d0c2a
selfup <boudinot.regis@yahoo.com>
2019-07-20 09:43:35 -0500

    add sync script for both repos

---
+++ scripts/sync.push.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+ORIGIN=grep git@github.com:selfup/gdsm
+
+git remote -v | grep $ORIGIN
+
+if [[ $? == "0" ]]
+then
+  git remote add origin $ORIGIN
+  git push -u origin master
+else
+  git push origin master
+fi
+
+./scripts/release.sh
